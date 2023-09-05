c037f91367827217b376e25e3fdacfe7692ae62a
Hanzen <heshou@ucsd.edu>
2023-09-05 13:08:39 -0700

    Dacite dependency

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='awsed-client',
     extras_require=dict(tests=['pytest']),
+    install_requires=['dacite'],
     packages=find_packages(where="src"),
     package_dir={"": "src"},
 )
